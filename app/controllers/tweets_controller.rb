4716984dd23a83e36a4a5f30d8b75d0489722782
Yin Lee <leesrrh@gmail.com>
2024-01-15 10:35:29 +0900

    added turbo_steram

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,30 @@
 class TweetsController < ApplicationController
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.all.order(created_at: :asc)
   end
 
   def create
+    @tweet = Tweet.new(tweet_params.merge(user_id: current_user))
+    @tweet.user = current_user
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_message
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
